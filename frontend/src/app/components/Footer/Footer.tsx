c5e5b76610e0076e9d209a9e13af7679a2596e00
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-11-28 12:35:45 +0000

    Update Footer

--- frontend/src/app/components/Footer/Footer.tsx
+++ frontend/src/app/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.footerText}>2025 Stashy. Created by Kit Jones with some help from Alex.</p>
+      <p className={styles.footerText}>2025 Stashy. Created by Kit Jones.</p>
     </footer>
   );
 }
